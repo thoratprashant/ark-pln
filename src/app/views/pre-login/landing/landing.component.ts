f57437e8aa2728f13744666eda6bfa6f025a0b1e
thoratprashant <prashant.thorat@arkenea.com>
2021-08-07 00:07:51 +0530

    Working on landing

--- src/app/views/pre-login/landing/landing.component.ts
+++ src/app/views/pre-login/landing/landing.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  tdatas: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.tdatas = [
+      {
+        iconName: 'thumb_up',
+        heading: 'Greater Profits',
+        info: 'With industry leading leverage options, you risk less of your own money and get to realize more of the profits.'
+      },
+      {
+        iconName: 'thumb_up',
+        heading: 'Housing Shortage, Solved',
+        info: 'The only way to solve a housing shortage is to bring on new inventory. With products designed specifically for builders and investors, you can do just that.'
+      },
+      {
+        iconName: 'thumb_up',
+        heading: 'Relationships > Deals',
+        info: 'Deals pay the bills, but relationships solidify careers. By placing an emphasis on the relationship, more deals get done.'
+      }
+    ]
   }
 
 }
