e33e8b2dc129b7dff3a3426d83696677c6fc5b62
dinzy17 <dineshnarendrapatil@gmail.com>
2021-08-12 07:39:47 +0530

    completed the layout but have some issues

---
+++ src/app/broker/broker.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-broker',
+  templateUrl: './broker.component.html',
+  styleUrls: ['./broker.component.scss']
+})
+export class BrokerComponent implements OnInit {
+
+  hide = true;  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
