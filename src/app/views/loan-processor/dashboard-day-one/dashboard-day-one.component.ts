ddd586d58ba7b0c680153728b9f7fcd818b25f4d
thoratprashant <prashant.thorat@arkenea.com>
2021-08-20 18:05:34 +0530

    Completed day-one scenario > LP

--- src/app/views/loan-processor/dashboard-day-one/dashboard-day-one.component.ts
+++ src/app/views/loan-processor/dashboard-day-one/dashboard-day-one.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-day-one.component.scss']
 })
 export class DashboardDayOneComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
+      
   }
 
 }
